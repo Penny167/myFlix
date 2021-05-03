47a02b054f08df7ad2cd866135651ba200b5a4cd
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-05-03 13:50:15 +0100

    Set up server and routiing for JSON data

--- index.js
+++ index.js
@@ -34,3 +34,13 @@ let topTenMovies = [
     name: 'Moonstruck'
   }
 ];
+
+// Setting up the server
+app.listen(8080,() => {
+  console.log('The server is listening on port 8080')
+});
+
+// Routing the request for the JSON object
+app.get('/movies',(req, res) => {
+  res.json(topTenMovies);
+});

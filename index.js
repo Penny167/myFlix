832fcb2174a212d502f5a411aab3055fd5d9d6a4
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-05-03 13:37:00 +0100

    Added JSON object with top 10 movies

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 const express = require('express');
 const app = express();
+
+// Creating a JSON object with the names of my top 10 movies
+let topTenMovies = [
+  {
+    name: 'The Godfather'
+  },
+  {
+    name: 'The Fellowship of the Ring'
+  },
+  {
+    name: 'Out of Africa'
+  },
+  { 
+    name: 'Indochine'
+  },
+  {
+    name: 'Elizabeth'
+  },
+  {
+    name: 'Casablanca'
+  },
+  {
+    name: 'Doctor Zhivago'
+  },
+  {
+    name: 'Ghandi'
+  },
+  {
+    name: 'Amadeus'
+  },
+  {
+    name: 'Moonstruck'
+  }
+];

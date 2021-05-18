a9a1497c1d77993a638f749121a7c137ccf41073
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-05-18 13:02:32 +0100

    Created schema and models for movie and user collections.

--- models.js
+++ models.js
@@ -1,1 +1,30 @@
 const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  Genre: {
+    Name: {type: String, required: true},
+    Description: {type: String, required: true}
+  },
+  Director: {
+    Name: {type: String, required: true},
+    Bio: {type: String, required: true},
+    Birth: String,
+    Death: String
+  },
+  ImagePath: String,
+  Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavouriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+

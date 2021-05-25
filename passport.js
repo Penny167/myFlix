e6ec1a0db1ed6dbe3a6846812e8629be1491e22b
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-05-25 13:03:22 +0100

    Strategies completed for local and jwt strategies.

--- passport.js
+++ passport.js
@@ -9,16 +9,38 @@ passport.use(new LocalStrategy(
   // No options stated: Because our fields are called Username and Password we don't need to map these explicitly 
   // Verify callback takes username, password and invokes done where credentials are valid
   (username, password, done) => {
-    Users.findOne()
+    console.log(username + ' ' + password);
+    Users.findOne({Username: username}, (err, user) => {
+      if (err) {
+        console.log(err);
+        return done(err);
+      }
+      if (!user) {
+        console.log('Invalid username.');
+        return done(null, false, {message: 'Invalid username.'});
+      }
+      console.log('Authentication complete.');
+      return done(null, user);
+    });
   }
-  )
-);
+));
 
 passport.use(new JWTStrategy(
-  // options
-  {jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey = 'secret'
-  },
-  //verify
-  )
-)
+  // Options object requires the function to return the JWT and the secret to decode it
+  {jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(), secretOrKey: 'secret'},
+  //Verify callback takes decoded JWT payload and invokes done where userid valid
+  (jwt_payload, done) => {
+    Users.findOne({id: jwt_payload.sub}, (err, user) => {
+      if (err) {
+        console.log(err);
+        return done(err);
+      }
+      if (!user) {
+        console.log('Invalid username.')
+        return done(null, false, {message: 'Invalid username.'})
+      } 
+      console.log('Request authenticated.');
+      return done(null, user)
+    });
+  }
+));

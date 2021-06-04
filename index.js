f96e93720005d80bc55f823169d9c5d3e2df32fd
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-06-04 11:01:05 +0100

    Updated user registration handler to hash password when user is created and store hashed version in the database.

--- index.js
+++ index.js
@@ -85,6 +85,7 @@ app.get('/movies/director/:Name', passport.authenticate('jwt', {session: false})
 
 // Register a new user
 app.post('/users', (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({Username: req.body.Username})
   .then((user) => {
     if(user) {
@@ -92,7 +93,7 @@ app.post('/users', (req, res) => {
     } else {
       Users.create({
         Username: req.body.Username,
-        Password: req.body.Password,
+        Password: hashedPassword,
         Email: req.body.Email,
         Birthday: req.body.Birthday
       })
